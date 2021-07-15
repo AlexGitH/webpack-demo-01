5a0aa8c3bd150ffd51a6a9bd31eccbbf40fa3a69
AlexP <alexander.pristavka@gmail.com>
2021-07-15 23:40:05 +0300

    Dynamic Imports;

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import _ from 'lodash';
+function getComponent() {
 
-function component() {
-  const element = document.createElement('div');
+  return import('lodash')
+    .then(({default: _ }) => {
+      const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+      return element;
+    })
+    .catch((error)=>'An error occurred while loading the component');
 }
 
-document.body.appendChild(component());
+getComponent().then( (component)=> {
+  document.body.appendChild( component );
+})
